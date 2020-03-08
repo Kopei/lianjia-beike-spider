5ada96af16f7e143c2f6e8286bc830e167e55aa2
cunpei nan <nan_cunpei@wuxinextcode.com>
2020-03-08 13:40:01 +0800

    ADD LOCATION QUERY FORM BAIDU LBS

--- lib/utility/location.py
+++ lib/utility/location.py
@@ -1,23 +1,55 @@
 # -*- coding:utf-8 -*-
+import os
+import time
 from urllib.request import urlopen
 from urllib.parse import quote
 import json
+
+import threadpool
+import pandas as pd
+
+# from lib.spider.base_spider import SPIDER_NAME
+# from lib.utility.date import get_date_string
+# from lib.utility.path import DATA_PATH
+# from lib.zone.city import get_city, get_chinese_city
+
+POOL_SIZE = 512
 
 
 def getjwd_bd(address):
     """根据地址获得经纬度（百度）"""
     url = 'http://api.map.baidu.com/geocoding/v3/?address='
     output = 'json'
-    ak = 'FAjgfSoSquGTrL5cedE50HxhTl7EUqN7'#需填入自己申请应用后生成的ak
-    add = quote(address) #本文城市变量为中文，为防止乱码，先用quote进行编码
-    url2 = url+add+'&output='+output+"&ak="+ak
+    ak = 'FAjgfSoSquGTrL5cedE50HxhTl7EUqN7'  # 需填入自己申请应用后生成的ak
+    add = quote(address)  # 本文城市变量为中文，为防止乱码，先用quote进行编码
+    url2 = url + add + '&output=' + output + "&ak=" + ak
     req = urlopen(url2)
     res = req.read().decode()
     temp = json.loads(res)
-    lng = float(temp['result']['location']['lng'])  # 经度 Longitude  简写Lng
-    lat = float(temp['result']['location']['lat'])  # 纬度 Latitude   简写Lat
+    if temp.get('result'):
+        lng = float(temp['result']['location']['lng'])  # 经度 Longitude  简写Lng
+        lat = float(temp['result']['location']['lat'])  # 纬度 Latitude   简写Lat
+    else:
+        lng = 0
+        lat = 0
     return lng, lat
 
 
 if __name__ == '__main__':
-    print(getjwd_bd('中远两湾城'))
+    addressDf = pd.read_csv(
+        '/Users/ncpbestgmail.com/github/lianjia-beike-spider/data/ke/xiaoqu/sh/20200307/baoshan_dachangzhen.csv',
+        header=None, names=['date', 'district', 'area', 'xiaoqu', 'price', 'sale', 'ninetydaysale', 'buildyear',
+                            'currentonrent'])
+
+    addresses = addressDf['xiaoqu']
+    t1 = time.time()
+    pool = threadpool.ThreadPool(POOL_SIZE)
+    my_requests = threadpool.makeRequests(getjwd_bd, addresses)
+    [pool.putRequest(req) for req in my_requests]
+    pool.wait()
+    pool.dismissWorkers(POOL_SIZE, do_join=True)  # 完成后退出
+    t2 = time.time()
+    print(t2-t1)
+    for i in addresses:
+        getjwd_bd(i)
+    print(time.time()-t2)
